21c3f23c4e0814783d841aa2d9bd498fd01dd343
stevengeeky <stevengeeky@gmail.com>
2018-06-07 08:42:26 -0400

    fixed default key assignment

--- util.js
+++ util.js
@@ -1093,7 +1093,7 @@ function runApp(headers, opt) {//appSearch, userInputs, projectSearch, resourceS
                         break;
                 }
                 
-                values[key] = opt.config[key];
+                values[key] = userParam;
             }
         }
 
